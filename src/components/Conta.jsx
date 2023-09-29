a08d6f820d72752d06d94050624f06988486141a
Elany Peixoto <elanysantos5@outlook.com>
2023-09-29 18:38:06 -0300

    bug fix

--- src/components/Conta.jsx
+++ src/components/Conta.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import './styles.css';
+
 function Boletos({ contas }) {
 
     return (
         <div className="d-flex justify-content-center align-items-center vh-100">
             {contas ? (
-                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                <div className="row">
                     {contas.map((conta, index) => (
                         <div key={index} className="col">
-                            <div className={`card text-white bg-${index % 2 === 0 ? 'primary' : 'info'} mb-3`} style={{ maxWidth: '18rem' }}>
+                            <div className="card card-custom">
                                 <div className="card-body">
                                     <h5 className="card-title">Nº da Cliente: {conta.cliente_id}</h5>
                                     <p className="card-text">{conta.cliente.nome}</p>
                                 </div>
                                 <ul className="list-group list-group-flush">
-                                    <li className={`list-group-item bg-${index % 2 === 0 ? 'primary' : 'info'}`}>Nº da Unidade: {conta.unidade.id}</li>
-                                    <li className={`list-group-item bg-${index % 2 === 0 ? 'primary' : 'info'}`}>Endereço: {conta.unidade.endereco}</li>
-                                    <li className={`list-group-item bg-${index % 2 === 0 ? 'primary' : 'info'}`}>Reference: {conta.reference}</li>
-                                    <li className={`list-group-item bg-${index % 2 === 0 ? 'primary' : 'info'}`}>kwh: {conta.kwh}</li>
-                                    <li className={`list-group-item bg-${index % 2 === 0 ? 'primary' : 'info'}`}>Vencimento: {conta.vecimento}</li>
-                                    <li className={`list-group-item bg-${index % 2 === 0 ? 'primary' : 'info'}`}>Valor: {conta.valor}</li>
+                                    <li className="list-group-item">Nº da Unidade: {conta.unidade.id}</li>
+                                    <li className="list-group-item">Endereço: {conta.unidade.endereco}</li>
+                                    <li className="list-group-item">Reference: {conta.reference}</li>
+                                    <li className="list-group-item">kwh: {conta.kwh}</li>
+                                    <li className="list-group-item">Vencimento: {conta.vecimento}</li>
+                                    <li className="list-group-item">Valor: {conta.valor}</li>
                                 </ul>
                             </div>
                         </div>
@@ -28,6 +30,7 @@ function Boletos({ contas }) {
                 <></>
             )}
         </div>
+
     )
 }
 
